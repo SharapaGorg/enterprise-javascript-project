d54f283343b910f6c7e182184fc6a72264d41f98
Anna Serova <kvassoedik@gmail.com>
2025-10-18 14:26:49 +0500

    chore: eslint, prettier setup (#3)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,22 @@
 // vitest.config.ts
-import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      provider: "v8",
+      reporter: ["text", "json", "html"],
       exclude: [
-        'node_modules/**',
-        'tests/**',
-        '*.config.*',
-        '.nuxt/**',
-        'dist/**',
+        "node_modules/**",
+        "tests/**",
+        "*.config.*",
+        ".nuxt/**",
+        "dist/**",
       ],
     },
   },
-})
+});
